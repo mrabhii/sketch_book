851b9b5bda55a955256e2a8b61d96df3f66b8319
mrabhii <mrabhii8526@gmail.com>
2023-10-22 02:12:48 +0530

    deployments

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 const URL = process.env.NODE_ENV === 'production' ? 'https://abhiisketch-book-server.onrender.com' : 'http://localhost:5000'
 export const socket = io(URL);
-// export const socket =io('http://localhost:5000')
+// export const socket =io('http://localhost:5000')atus
